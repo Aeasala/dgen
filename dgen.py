1bef97879dda1ecfb39905fda0a85ba3cf0bf009
Aeasala <esmacdon@gmail.com>
2022-10-15 13:46:24 -0400

    first implementation of some file writing functionality

--- dgen.py
+++ dgen.py
@@ -5,4 +5,27 @@ Created on Sat Oct 15 12:59:32 2022
 
 @author: evan
 """
+import sys,shutil,cmd
+from helpers.appinfo import appinfo
 
+app = appinfo()
+
+print("What is the name of your application?\n")
+
+lastuserin = input("app.Name > ")
+app.required["Name"]=lastuserin
+print("_____________")
+print("What is the path of your application? (use as short of an arg as possible, using ~ or ./ if preferred)\n")
+
+lastuserin = input("app.Path > ")
+app.required["Path"]=lastuserin
+
+print("Inside that path, what command do you want to run? (quotes are safe)")
+
+lastuserin = input("app.Exec > ")
+app.required["Exec"]=lastuserin
+
+app.tellMeEverything()
+app.appendToFile()
+input()
+#shutil.copyfile("./template.desktop","./new.desktop")
